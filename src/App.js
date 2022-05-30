472996e92160f5b6cb7442b360f815414ad25bc9
Kristinna97 <kristina.petrova13@gmail.com>
2022-05-30 21:57:43 +0100

    style game over fieeld and reset button

--- src/App.js
+++ src/App.js
@@ -12,12 +12,24 @@ function App() {
  
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  const [playable, setPlayable] = useState(true)
+
+ function playAgain(){
+  setPlayable(true);
+  setCorrectLetters([]);
+  setWrongLetters([]);
+  const buttons = document.querySelectorAll('button')
+  buttons.forEach(button =>{
+    button.disabled = false
+  })
+  }
+ 
 
   return (
     <div className="App">
     <Header/>
-    <Hangman wrongLetters={wrongLetters}/>
-    <WordAndAlphabet word={selectedWord} wrongLetters={wrongLetters} setWrongLetters={setWrongLetters} correctLetters={correctLetters} setCorrectLetters={setCorrectLetters}/>
+    <Hangman wrongLetters={wrongLetters} playable={playable} playAgain={playAgain} />
+    <WordAndAlphabet word={selectedWord} wrongLetters={wrongLetters} setWrongLetters={setWrongLetters} correctLetters={correctLetters} setCorrectLetters={setCorrectLetters} setPlayable={setPlayable}/>
     </div>
   );
 }
