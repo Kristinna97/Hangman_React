b14c71d0134ff08c227145fbc0c33c31114fe827
Kristinna97 <kristina.petrova13@gmail.com>
2022-05-30 17:33:56 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Header from './Components/Header';
+import Hangman from './Components/Hangman';
+import WordAndAlphabet from './Components/WordAndAlphabet';
+import { useState } from "react";
 import './App.css';
 
+const words = ['research', 'application' , 'programming']
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  
+ 
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <div className="App">
     <Header/>
+    <Hangman wrongLetters={wrongLetters}/>
+    <WordAndAlphabet word={selectedWord} wrongLetters={wrongLetters} setWrongLetters={setWrongLetters} correctLetters={correctLetters} setCorrectLetters={setCorrectLetters}/>
     </div>
   );
 }
