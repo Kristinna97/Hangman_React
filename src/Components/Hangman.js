f6a114ae2d0e53b84d03a8dd35c0f158b3f082e0
Kristinna97 <kristina.petrova13@gmail.com>
2022-05-31 12:26:35 +0100

    add alt to image

--- src/Components/Hangman.js
+++ src/Components/Hangman.js
@@ -7,7 +7,7 @@ function Hangman({wrongLetters, playable, playAgain ,won, word }) {
    return (
        <>
        <div className='main'>
-        <p className="hangman"><img src={require(`../images/${wrongLetters.length}.png`)}></img></p>
+        <p className="hangman"><img alt={require(`${wrongLetters.length}-step-img`)} src={require(`../images/${wrongLetters.length}.png`)}></img></p>
         <GameOutcome wrongLetters={wrongLetters} playable={playable}  playAgain={playAgain} won={won} word={word}/>
         </div>
         </>
